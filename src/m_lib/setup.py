c433ddc2bd5b0fe8141e6f8e63bf4dec626c4998
Jialiang Zhu <v-jialzhu@microsoft.com>
2023-09-27 15:19:30 +0000

    bug fix

--- src/m_lib/setup.py
+++ src/m_lib/setup.py
@@ -1,6 +1,7 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import numpy
 
 ext_modules=[ Extension("pictorial",
               ["pictorial.pyx"],
@@ -10,4 +11,6 @@ ext_modules=[ Extension("pictorial",
 setup(
   name = "pictorial",
   cmdclass = {"build_ext": build_ext},
-  ext_modules = ext_modules)
+  ext_modules = ext_modules,
+  include_dirs=[numpy.get_include()]
+  )
