8b00782f4e5a104563e27f7c2bdd7189d03acc03
Jialiang Zhu <v-jialzhu@microsoft.com>
2023-09-27 16:24:09 +0000

    xx

--- run.bash
+++ run.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 while true; do
-    CUDA_VISIBLE_DEVICES=0 python src/tools/preprocess.py -d Campus -dump_dir ./datasets/Campus_processed
+    CUDA_VISIBLE_DEVICES=0 python ./src/m_utils/evaluate.py -d Campus
     sleep 1  # 等待一秒后重新运行命令，可以根据需要调整等待时间
 done
