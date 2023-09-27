f78f364b1b1dd29df2ec50eaf4a47cb09d77c0f7
Jialiang Zhu <v-jialzhu@microsoft.com>
2023-09-27 15:45:11 +0000

    xx

--- src/m_utils/evaluate.py
+++ src/m_utils/evaluate.py
@@ -204,4 +204,5 @@ if __name__ == '__main__':
             test_actor3D /= 100  # mm->m
         poses3ds = evaluate ( test_model, test_actor3D, test_range, test_loader, is_info_dicts=bool ( args.dumped_dir ),
                    dump_dir=osp.join ( project_root, 'result' ) )
-        np.save (  f'logs/{dataset_name}_{seq}_{cam}_poses3ds.npy', poses3ds )
+        if dataset_name == 'panoptic':
+            np.save (  f'logs/{dataset_name}_{seq}_{cam}_poses3ds.npy', poses3ds )
